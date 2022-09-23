2be3a5fe861bd4ed5c752575dc8dd0347afc2e23
asutosh99 <asumanu99@gmail.com>
2022-09-23 22:45:19 +0530

    code cleanup

--- src/Charts/BarChart.js
+++ src/Charts/BarChart.js
@@ -3,7 +3,6 @@ import ReactEcharts from "echarts-for-react";
 import data from "../Wine-Data.json";
 
 function BarChart() {
-  // console.log(data[0]);
 
   const alcoholMalicAcidAvgMap = new Map();
   const MalicAcid1 = "Malic Acid";
@@ -16,30 +15,22 @@ function BarChart() {
         : data[i][MalicAcid1]
     );
   }
-  //console.log(alcoholMalicAcidAvgMap);
   const alcoholType = alcoholMalicAcidAvgMap.size;
-  // console.log(alcoholType);
   let occurance = [alcoholType + 1];
   for (let i = 0; i <= alcoholType; ++i) occurance[i] = 0;
   for (let i = 0; i < data.length; i++) {
     occurance[data[i].Alcohol]++;
   }
-  // console.log(occurance);
 
   for (const [key, value] of alcoholMalicAcidAvgMap) {
-    console.log(key, value);
     alcoholMalicAcidAvgMap.set(key, value / occurance[key]);
   }
-  // console.log(alcoholMalicAcidAvgMap);
   let xAxis = [];
   let yAxis = [];
   for (const [key, value] of alcoholMalicAcidAvgMap) {
-    console.log(key, value);
     xAxis.push(key);
     yAxis.push(value);
   }
-  // console.log(xAxis);
-  //console.log(yAxis);
 
   const option = {
     grid: {},
@@ -55,7 +46,7 @@ function BarChart() {
       },
       type: "category",
       data: xAxis,
-      name: "Alcohol"
+      name: "Alcohol",
     },
     yAxis: {
       axisLabel: {
@@ -68,7 +59,7 @@ function BarChart() {
     series: [
       {
         data: yAxis,
-        type: "bar"
+        type: "bar",
       },
     ],
   };
